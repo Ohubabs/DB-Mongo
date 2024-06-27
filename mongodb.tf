56bd3a83b2b7500aa45d7c9ebe39d302a04e5e96
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-06-27 09:09:23 -0700

    changed create namespace to true and updated comment

--- mongodb.tf
+++ mongodb.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "mongodb" {
   name       = "mongodb"
-  #create_namespace = true
-  namespace  = "springapp" #make sure that a namespace is created before deployment otherwise there'll be an error in downloading the chart.
+  create_namespace = true
+  namespace  = "springapp" #make sure this matches the same namespace for you java springapp application deployment
   repository = "oci://registry-1.docker.io/bitnamicharts"
   chart      = "mongodb"
   values = [
